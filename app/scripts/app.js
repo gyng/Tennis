9c0e0e9f2b8119f5b84322739d0bcf942546121f
Ng Guoyou <fauxface+github@gmail.com>
2015-03-25 04:33:25 +0800

    Improve sensor data collection
    Async, basic thresholding

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,7 +7,7 @@
     this.dataHistory = [];
     this.historySize = 60;
     // this.pullEvery = 16;
-    this.pullEvery = 1000;
+    this.pullEvery = 100;
 
     this.ui.app = this;
 
@@ -16,25 +16,42 @@
 
   App.prototype = {
     pull: function (url) {
-      var xmlHttp = new XMLHttpRequest();
-      xmlHttp.open("GET", url, false);
-      xmlHttp.send(null);
-      var data = JSON.parse(xmlHttp.responseText);
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url, true);
 
-      return data;
+      xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            var data = JSON.parse(xhr.responseText);
+            this.process(data);
+          } else {
+            console.error(xhr.statusText);
+          }
+        }
+      }.bind(this);
+
+      xhr.send(null);
     },
 
-    start: function (url) {
-      window.setInterval(function (url) {
-        this.process(this.pull(url));
-      }.bind(this), this.pullEvery, url);
+    start: function () {
+      console.log("started");
+      this.pullIntervalId = window.setInterval(function () {
+        this.pull(this.url);
+      }.bind(this), this.pullEvery);
+    },
+
+    stop: function () {
+      if (this.pullIntervalId !== null && typeof this.pullIntervalId !== "undefined") {
+        window.clearInterval(this.pullIntervalId);
+        this.pullIntervalId = null;
+      }
     },
 
     process: function (data) {
       this.dataHistory.push(data);
       this.dataHistory = this.dataHistory.slice(this.historySize);
 
-      // debug
+      // debug, threshold is not good
       var laValues = _.find(data.sensors, function (s) {
         return s.type === "linear_acceleration";
       }).values;
@@ -43,35 +60,58 @@
         return s.type === "orientation";
       }).values;
 
-      this.ui.updateServeValues(laValues[0], oValues[0], oValues[1]);
+      var force = Math.sqrt(
+        laValues[0] * laValues[0] +
+        laValues[1] * laValues[1] +
+        laValues[2] * laValues[2]
+      );
+
+      var threshold = 15;
+      if (force > threshold) {
+        this.ui.updateServeValues(oValues[0], oValues[1], force);
+      }
     }
   };
 
   function UI (root) {
     this.root = root;
-    this.frontAngleEl = this.root.querySelector("#sensor-forward-angle-value");
-    this.sideAngleEl = this.root.querySelector("#sensor-side-angle-value");
-    this.forceEl = this.root.querySelector("#sensor-force-value");
+    this.frontAngleEl = this.root.querySelector(".sensor-result.forward-angle");
+    this.sideAngleEl = this.root.querySelector(".sensor-result.side-angle");
+    this.forceEl = this.root.querySelector(".sensor-result.force");
+    this.serveHistory = [];
 
     this.urlFormEl = this.root.querySelector("#url-form");
     this.createUrlListener();
-
-    this.history = [];
 
     this.setupNav();
   }
 
   UI.prototype = {
     updateServeValues: function (forwardAngle, sideAngle, force) {
-      this.frontAngleEl.innerHTML = forwardAngle.toPrecision(3);
-      this.sideAngleEl.innerHTML = sideAngle.toPrecision(3);
-      this.forceEl.innerHTML = force.toPrecision(3);
+      this.frontAngleEl.querySelector(".sensor-number").innerHTML = forwardAngle.toPrecision(3);
+      this.sideAngleEl.querySelector(".sensor-number").innerHTML = sideAngle.toPrecision(3);
+      this.forceEl.querySelector(".sensor-number").innerHTML = force.toPrecision(3);
+
+      if (this.serveHistory.length > 0) {
+        var previousServe = this.serveHistory[this.serveHistory.length - 1];
+        this.frontAngleEl.querySelector(".sensor-comparison").innerHTML = previousServe.forwardAngle.toPrecision(3);
+        this.sideAngleEl.querySelector(".sensor-comparison").innerHTML = previousServe.sideAngle.toPrecision(3);
+        this.forceEl.querySelector(".sensor-comparison").innerHTML = previousServe.force.toPrecision(3);
+      }
+
+      this.serveHistory.push({
+        forwardAngle: forwardAngle,
+        sideAngle: sideAngle,
+        force: force
+      });
     },
 
     createUrlListener: function () {
       this.urlFormEl.onsubmit = function (e) {
         e.preventDefault();
         this.app.url = this.getUrl();
+        this.app.stop();
+        this.app.start();
       }.bind(this);
     },
 
@@ -88,13 +128,22 @@
       }.bind(this);
 
       Array.prototype.forEach.call(this.root.querySelectorAll(".button"), function (el) {
-
         if (el.dataset.goto !== null && typeof el.dataset.goto !== "undefined") {
           el.addEventListener("click", function () {
             this.navigate(el.dataset.goto);
           }.bind(this));
         }
+
+        if (el.dataset.fn !== null && typeof el.dataset.fn !== "undefined") {
+          el.addEventListener("click", function () {
+            this[el.dataset.fn]();
+          }.bind(this));
+        }
       }.bind(this));
+    },
+
+    startApp: function () {
+      this.app.start();
     },
 
     getUrl: function () {
@@ -111,8 +160,6 @@
     },
 
     loadPage: function (pageSelector) {
-
-      console.log("going to ", pageSelector)
       Array.prototype.forEach.call(this.root.querySelectorAll(".page"), function (el) {
         el.style.display = "none";
       });
