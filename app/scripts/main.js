e69bcbb984c64efbf0021d6046e247a20f4e6b71
Ng Guoyou <fauxface+github@gmail.com>
2015-04-08 01:36:02 +0800

    Clean up script loading sequence

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,16 @@
-require(["scripts/app.js", "scripts/ui.js"], function () {
+(function () {
   "use strict";
 
-  var ui = new UI(document);
-  new App(ui);
-});
+  require.config({
+    paths: {
+      editablegrid: "editablegrid-2.0.1",
+    }
+  });
+
+  require(["lodash", "editablegrid"], function (_, EditableGrid) {
+    require(["app", "ui"], function () {
+      var ui = new UI(document);
+      new App(ui);
+    });
+  });
+}());
